6beee4ca18909e25d248848ead58ef5f5db5968c
Nisha271 <bukkenisha@gmail.com>
2020-11-03 19:00:00 +0530

    [Archana] Added: UC4 with to remove operation

--- src/com/blz/stacksqueuesimplementationbylinkedlist/QueuesOperation.java
+++ src/com/blz/stacksqueuesimplementationbylinkedlist/QueuesOperation.java
@@ -3,43 +3,64 @@ package com.blz.stacksqueuesimplementationbylinkedlist;
 public class QueuesOperation {
 
 	public int size;
-	public Node front,rear;
-	
+	public Node front, rear;
+
 	public QueuesOperation() {
 		size = 0;
 		front = null;
 		rear = null;
 	}
+
 	public void insert(int data) {
-		Node newNode = new Node(data,null);
-		
-		if(rear == null) {
+		Node newNode = new Node(data, null);
+
+		if (rear == null) {
 			rear = newNode;
 			front = newNode;
-		}else {
+		} else {
 			rear.setNextRefernce(newNode);
 			rear = rear.getNextReference();
 		}
 		size++;
 	}
+
+	public boolean isEmpty() {
+		return front == null;
+	}
+
+	public int dequeue() {
+		if (isEmpty()) {
+			System.out.println("Queue is Empty Cannot dequeue ");
+		}
+		Node temp = front;
+		front = temp.getNextReference();
+		size--;
+		if(front == null) {
+			rear = null;
+		}
+		return temp.getData();
+	}
+
 	public void display() {
 		System.out.println("Queue : ");
-		if(rear == null) {
+		if (rear == null) {
 			System.out.println("Queue is Empty");
 		}
-		while(front !=rear.getNextReference()) {
-			System.out.println(front.getData());
-			front = front.getNextReference();
+		Node temp = front;
+		while (temp != rear.getNextReference()) {
+			System.out.println(temp.getData());
+			temp = temp.getNextReference();
 		}
 	}
+
 	public static void main(String[] args) {
 		QueuesOperation queueOperation = new QueuesOperation();
 		queueOperation.insert(56);
 		queueOperation.insert(30);
 		queueOperation.insert(70);
 		queueOperation.display();
-		
-		
+		System.out.println("Dequeue Element :" + queueOperation.dequeue());
+		queueOperation.display();
 	}
 
 }
